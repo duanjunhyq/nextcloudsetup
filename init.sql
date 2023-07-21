9e131d55f2c725e68501f8a8ff760c998d3a9477
Ubuntu <ubuntu@nextcloud.arbutus>
2023-07-21 21:50:06 +0000

    fix memcache issues

--- init.sql
+++ init.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE IF NOT EXISTS nextcloud;
-GRANT ALL PRIVILEGES ON nextcloud.* TO 'your_username'@'%';
-GRANT ALL PRIVILEGES ON nextcloud.* TO 'your_username'@'localhost';
+GRANT ALL PRIVILEGES ON nextcloud.* TO 'nextcloud'@'%';
+GRANT ALL PRIVILEGES ON nextcloud.* TO 'nextcloud'@'localhost';
 
